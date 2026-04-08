6de04d32e5505b4f6a9f61ba6e40ed7d8237fc65
sampurnamondal004 <mondalsampurna012@gmail.com>
2026-04-08 23:19:53 +0530

    fix:models.py and reset and step

--- server/app.py
+++
@@ -1,6 +0,0 @@
-from social_media_env.server.app import app
-
-if __name__ == "__main__":
-    import uvicorn, os
-    port = int(os.environ.get("PORT", 7860))
-    uvicorn.run("server.app:app", host="0.0.0.0", port=port)
